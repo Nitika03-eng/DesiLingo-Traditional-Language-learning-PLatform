f5bf418195f10b7ea51c4ccd627411cb32e4d729
Nitika03-eng <nitikadung7@gmail.com>
2025-09-26 16:35:47 +0530

    fixed

--- app.py
+++ app.py
@@ -346,5 +346,7 @@ def roadmap():
     return render_template("roadmap.html")
 
 # ----------------- RUN APP ----------------- #
+
 if __name__ == "__main__":
-    app.run(debug=True)
+    port = int(os.environ.get('PORT', 5000))  # Default to 5000 locally
+    app.run(host='0.0.0.0', port=port, debug=False)
